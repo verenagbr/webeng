bfaf123bfe8b4ec68be8210e646b5af1aa0f8618
Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>
2020-05-28 23:28:27 +0200

    prak04: 2.5
    Signed-off-by: Verena Gebauer <verena.heinze@study.hs-duesseldorf.de>

--- webengShop/src/main/java/de/hsduesseldorf/webengshop/access/MockArticleDAO.java
+++ webengShop/src/main/java/de/hsduesseldorf/webengshop/access/MockArticleDAO.java
@@ -1,4 +1,30 @@
 package de.hsduesseldorf.webengshop.access;
 
+import de.hsduesseldorf.webengshop.Article;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class MockArticleDAO {
+
+    private List<Article> articles = new ArrayList<>(
+            Arrays.asList(
+                    new Article(UUID.randomUUID(), "Test Article 1", 1.34f, 0),
+                    new Article(UUID.randomUUID(), "Test Article 2", 1.34f, 0),
+                    new Article(UUID.randomUUID(), "Test Article 3", 1.34f, 4),
+                    new Article(UUID.randomUUID(), "Test Article 4", 1.34f, 0)
+                         )
+    );
+
+    Article getArticle(UUID uuid) {
+        for (final Article article : articles) {
+            if (article.getUuid().equals(uuid)) {
+                return article;
+            }
+        }
+
+        return null;
+    }
 }
